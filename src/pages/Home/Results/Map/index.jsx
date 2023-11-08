e83f6db45abae54bbd67f64dbdb9d4df3587e8d5
domenicodeblasi <domenico.deblasi33@gmail.com>
2023-11-08 19:58:43 +0100

    fixed bugs in Leaflet Map, added a custom marker to replace the previous default one

--- src/pages/Home/Results/Map/index.jsx
+++ src/pages/Home/Results/Map/index.jsx
@@ -3,14 +3,19 @@ import "./map-styles.css"
 import "leaflet/dist/leaflet.css"
 import { useForm } from "../../../../context/FormContext"
 import { MapContainer, TileLayer, Marker } from "react-leaflet"
-import { Icon } from "leaflet"
+import L from "leaflet"
 import MarkerIcon from "./../../../../assets/imgs/marker.svg"
 
 const Map = () => {
 
     const { inputs } = useForm()
-
     const position = [inputs.latitude, inputs.longitude]
+
+    const customIcon = L.icon({
+        iconUrl: MarkerIcon,
+        iconSize: [40, 40],
+        iconAnchor: [20, 36],
+    })
 
     return (
         <MapContainer
@@ -23,6 +28,7 @@ const Map = () => {
             />
             <Marker
                 position={position}
+                icon={customIcon}
             ></Marker>
         </MapContainer>
     )
